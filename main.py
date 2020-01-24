5cecd4ae3cc2c50baad34faa3a28c8ae0f4a5c02
Frank Hui <huifrank@live.com>
2020-01-23 17:15:53 -0800

    Add !subscribe, resolves issue #42

--- main.py
+++ main.py
@@ -1,29 +1,36 @@
+import json
+
 from fbchat import log, Client
 import os
 from fbchat import Message
 from fbchat.models import *
 import traceback
+from datetime import datetime
+import time
+
 
 # Subclass fbchat.Client and override required methods
 class dentaku_bot(Client):
     def onMessage(self, author_id, message_object, thread_id, thread_type, **kwargs):
         if "!" in str(message_object.text)[0]:
             command = str(message_object.text).replace("!", "").split(" ")
-            parameters = {
-                "user": command[1:],
-                "author_id": author_id,
-                "message_object": message_object,
-                "thread_id": thread_id,
-                "thread_type": thread_type
-            }
+            command_index = 1
             if command[0] == '':
                 for each in command:
                     if each != '':
                         command = each
                         break
+                    command += 1
             else:
                 command = command[0]
             try:
+                parameters = {
+                    "user": command[command_index:],
+                    "author_id": author_id,
+                    "message_object": message_object,
+                    "thread_id": thread_id,
+                    "thread_type": thread_type
+                }
                 module = __import__(command)
                 new_command = getattr(module, command)
                 instance = new_command(parameters, client=self)
@@ -48,14 +55,30 @@ def export_env():
             if "\"" in line:
                 os.environ[line.split("=")[0].split(" ")[1]] = line[line.find("\"") + 1:line.rfind("\"")]
             else:
-                line = line.replace("export", "").replace(" ","")
+                line = line.replace("export", "").replace(" ", "")
                 line = line.split("=")
                 os.environ[line[0]] = line[1]
 
+
 export_env()
 client = dentaku_bot(os.getenv('EMAIL'), os.getenv('PASSWORD'))
-if client.uid == "100045950177697":
-    client.send(Message(text="Dentaku is online."), thread_id="100011229734236", thread_type=ThreadType.USER)
+
+if os.path.exists("database.json"):
+    with open("database.json", 'r') as file:
+        database = json.load(file)
+        threads = database['subscription']
+        if 'deployment' in database:
+            database['deployment'] += 1
+        else:
+            database['deployment'] = 0
+    with open('database.json','w') as file:
+        json.dump(database, file)
 else:
-    client.send(Message(text="Dentaku is online."), thread_id=client.uid, thread_type=ThreadType.USER)
+    database = {"deployment":0, "subscription":[]}
+    with open('database.json','w') as file:
+        json.dump(database, file)
+for thread in threads:
+    client.send(Message(
+        text="[" + datetime.now().strftime("%Y-%m-%d %-I:%M%p") + "] Dentaku deployed just now. #" + str(database['deployment'])),
+                thread_id=client.uid, thread_type=ThreadType.USER)
 client.listen()

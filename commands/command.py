1c394bee49cdf938fa86793dcdc7e2899d94fa73
Frank Hui <huifrank@live.com>
2020-01-25 11:01:07 -0800

    Add typing to help development

--- commands/command.py
+++ commands/command.py
@@ -1,16 +1,17 @@
 from fbchat import Client
+import fbchat
 
 class Command:
 
     def __init__(self, parameters, client: Client):
-        self.database = parameters['database']
-        self.user_params = parameters['user']
-        self.author_id = parameters['author_id']
-        self.message_object = parameters['message_object']
-        self.thread_id = parameters['thread_id']
-        self.thread_type = parameters['thread_type']
-        self.client = client
-        self.author = self.client.fetchUserInfo(self.author_id)[self.author_id]
+        self.database: dict = parameters['database']
+        self.user_params: list = parameters['user']
+        self.author_id: int = parameters['author_id']
+        self.message_object: fbchat.Message = parameters['message_object']
+        self.thread_id: int = parameters['thread_id']
+        self.thread_type: fbchat.ThreadType = parameters['thread_type']
+        self.client: fbchat.Client = client
+        self.author: fbchat.User = self.client.fetchUserInfo(self.author_id)[self.author_id]
         self.documentation = {
             "parameters": "",
             "function": ""

51314455df86d4a74801168c447873233e765db4
Frank Hui <huifrank@live.com>
2020-01-25 15:38:55 -0800

    Add inheritance of Action from action.py

--- commands/command.py
+++ commands/command.py
@@ -1,28 +1,10 @@
 from fbchat import Client
 import fbchat
+from action import Action
 
-class Command:
+
+class Command(Action):
 
     def __init__(self, parameters, client: Client):
-        self.database: dict = parameters['database']
+        Action.__init__(self, parameters, client)
         self.user_params: list = parameters['user']
-        self.author_id: int = parameters['author_id']
-        self.message_object: fbchat.Message = parameters['message_object']
-        self.thread_id: int = parameters['thread_id']
-        self.thread_type: fbchat.ThreadType = parameters['thread_type']
-        self.client: fbchat.Client = client
-        self.author: fbchat.User = self.client.fetchUserInfo(self.author_id)[self.author_id]
-        self.documentation = {
-            "parameters": "",
-            "function": ""
-        }
-        client.markAsDelivered(self.thread_id, self.message_object.uid)
-        client.markAsRead(self.thread_id)
-        self.define_documentation()
-
-    def run(self):
-        print("Running abstract command...")
-        return
-
-    def define_documentation(self):
-        return

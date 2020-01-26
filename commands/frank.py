91425e8666f7d992fec5ca86e75bdedbad1220ab
georgecwan <georgecwan@users.noreply.github.com>
2020-01-25 21:35:18 -0800

    frank

---
+++ commands/frank.py
@@ -0,0 +1,29 @@
+from commands.command import Command
+from fbchat import Message
+
+class frank(Command):
+    def run(self):
+        response_text = ""
+        try:
+            text = self.message_object.replied_to.text
+            for i in text.split(" "):
+                i = i[0] + i[2:]
+                response_text += i + " "
+        except AttributeError:
+            try:
+                for i in self.user_params:
+                    i = i[0]+i[2:]
+                    response_text += i+" "
+            except:
+                response_text = "*Indecipherable toxic junk*"
+        self.client.send(
+            Message(text=response_text, mentions=None),
+            thread_id=self.thread_id,
+            thread_type=self.thread_type
+        )
+
+    def define_documentation(self):
+        self.documentation = {
+            "parameters": "REPLIED_MESSAGE",
+            "function": "frank-ify a REPLIED_MESSAGE"
+        }

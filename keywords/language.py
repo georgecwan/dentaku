973fc7baa2819e9b88adb60b2caf4637c8d9f73e
georgecwan <georgecwan@users.noreply.github.com>
2020-01-27 23:31:27 -0800

    Uppercase space and enable--> yes

--- keywords/language.py
+++ keywords/language.py
@@ -8,7 +8,7 @@ class language(Keyword):
     def run(self):
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
         self.client.send(
-            Message(text="@" + self.author.first_name + "WATCH YOUR LANGUAGE", mentions=mentions),
+            Message(text="@" + self.author.first_name.upper() + " WATCH YOUR LANGUAGE", mentions=mentions),
             thread_id=self.thread_id,
             thread_type=self.thread_type
         )

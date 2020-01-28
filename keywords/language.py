acba3430b56fa805cb2ac0c37e7ae4491af327a4
georgecwan <georgecwan@users.noreply.github.com>
2020-01-27 22:09:02 -0800

    language

--- keywords/language.py
+++ keywords/language.py
@@ -1,13 +1,14 @@
 from fbchat import Message
+from fbchat import Mention
 from keywords.keyword import Keyword
 
 
 class language(Keyword):
 
     def run(self):
-
+        mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
         self.client.send(
-            Message(text="WATCH YOUR LANGUAGE", mentions=None),
+            Message(text="@" + self.author.first_name + "WATCH YOUR LANGUAGE", mentions=mentions),
             thread_id=self.thread_id,
             thread_type=self.thread_type
         )

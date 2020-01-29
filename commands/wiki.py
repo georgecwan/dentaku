b281803c86540acc9d273844e23dfdb83920e57a
georgecwan <georgecwan@users.noreply.github.com>
2020-01-28 18:32:14 -0800

    Fix !wiki errors

--- commands/wiki.py
+++ commands/wiki.py
@@ -8,23 +8,21 @@ class wiki(Command):
 
     def run(self):
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
-        resultfound = False
         if len(self.user_params) == 0:
-            response_text = "@" + self.author.first_name + " Here you go."
-            link = "https://en.wikipedia.org/wiki/Nothing"
+            response_text = "@" + self.author.first_name + " Here you go." + "\n" + "https://en.wikipedia.org/wiki/Nothing"
         else:
             try:
-                link = "https://wikipedia.org/w/index.php?search="+"_".join(self.user_params)
+                link = "https://en.wikipedia.org/w/index.php?search="+"_".join(self.user_params)
                 webpage = requests.get(link)
                 text = str(bs4.BeautifulSoup(webpage.text, 'html.parser').find("div", class_="mw-search-result-heading"))
-                if text == None:
+                if text == "None":
                     link = webpage.url
                 else:
                     text = text[text.find("href") + 6:]
                     text = text[:text.find("\"")]
-                    link = "https://wikipedia.org"+text
-                response_text = "@" + self.author.first_name + " Here's what I found for you on " + " ".join(self.user_params)
-                resultfound = True
+                    link = "https://en.wikipedia.org" + text
+                response_text = "@" + self.author.first_name + " Here's what I found for you about " + " ".join(self.user_params) \
+                                + "\n" + link
             except:
                 response_text = "@" + self.author.first_name + " Wikipedia cannot get a result."
 
@@ -34,18 +32,8 @@ class wiki(Command):
             thread_type=self.thread_type
         )
 
-        if resultfound == True:
-            self.send_link(link)
-
     def define_documentation(self):
         self.documentation = {
             "parameters": "SEARCH TERMS",
             "function": "Gives you the link to a Wikipedia article to satisfy your thirst for knowledge."
         }
-
-    def send_link(self, link):
-        self.client.send(
-            Message(text=link),
-            thread_id=self.thread_id,
-            thread_type=self.thread_type
-        )

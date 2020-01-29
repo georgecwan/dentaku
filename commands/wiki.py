c8be519fea8e3b8e50faa3d3399590049f863666
GcWan <GcWan@users.noreply.github.com>
2020-01-28 18:15:15 -0800

    !wiki

--- commands/wiki.py
+++ commands/wiki.py
@@ -9,20 +9,24 @@ class wiki(Command):
     def run(self):
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
         resultfound = False
-        try:
-            link = "https://wikipedia.org/w/index.php?search="+"_".join(self.user_params)
-            webpage = requests.get(link)
-            text = str(bs4.BeautifulSoup(webpage.text, 'html.parser').find("div", class_="mw-search-result-heading"))
-            if text == None:
-                link = webpage.url
-            else:
-                text = text[text.find("href") + 6:]
-                text = text[:text.find("\"")]
-                link = "https://wikipedia.org"+text
-            response_text = "@" + self.author.first_name + " Here's what I found for you on " + " ".join(self.user_params)
-            resultfound = True
-        except:
-            response_text = "@" + self.author.first_name + " Wikipedia cannot get a result."
+        if len(self.user_params) == 0:
+            response_text = "@" + self.author.first_name + " Here you go."
+            link = "https://en.wikipedia.org/wiki/Nothing"
+        else:
+            try:
+                link = "https://wikipedia.org/w/index.php?search="+"_".join(self.user_params)
+                webpage = requests.get(link)
+                text = str(bs4.BeautifulSoup(webpage.text, 'html.parser').find("div", class_="mw-search-result-heading"))
+                if text == None:
+                    link = webpage.url
+                else:
+                    text = text[text.find("href") + 6:]
+                    text = text[:text.find("\"")]
+                    link = "https://wikipedia.org"+text
+                response_text = "@" + self.author.first_name + " Here's what I found for you on " + " ".join(self.user_params)
+                resultfound = True
+            except:
+                response_text = "@" + self.author.first_name + " Wikipedia cannot get a result."
 
         self.client.send(
             Message(text=response_text, mentions= mentions),
@@ -31,7 +35,7 @@ class wiki(Command):
         )
 
         if resultfound == True:
-            self.send_link(self, link)
+            self.send_link(link)
 
     def define_documentation(self):
         self.documentation = {

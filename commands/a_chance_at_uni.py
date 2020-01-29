5ac3fe805fca17d520b235da3bfc2c52b918d29d
georgecwan <georgecwan@users.noreply.github.com>
2020-01-28 17:21:54 -0800

    Language bug fixes, chance at uni upgrade

--- commands/a_chance_at_uni.py
+++ commands/a_chance_at_uni.py
@@ -9,7 +9,10 @@ class a_chance_at_uni(Command):
         if self.author.first_name == "Charley":
             response_text = "Well @" + self.author.first_name + " I think your 12 rejections are statistically compelling enough..."
         elif len(self.user_params) > 0:
-            response_text = "@" + self.author.first_name + "\n u op guaranteed acceptance at " + self.user_params[0]
+            uni = ""
+            for i in self.user_params:
+                uni += i + " "
+            response_text = "@" + self.author.first_name + "\n u op guaranteed acceptance at " + uni
         else:
             response_text = "@" + self.author.first_name + "\n u op guaranteed acceptance at HYPSM"
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]

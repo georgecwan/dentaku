6f0bbc7dc0a8a508a61da59d13964d9bdbde382f
Frank Hui <huifrank@live.com>
2020-01-29 18:47:55 -0800

    Parse json response

--- commands/ngrok.py
+++ commands/ngrok.py
@@ -1,3 +1,5 @@
+import json
+
 from commands.command import Command
 from fbchat import Message
 from fbchat import Mention
@@ -10,7 +12,7 @@ class ngrok(Command):
         if 'ngrok-status' not in self.database or self.database['ngrok-status'] != 'on':
             p = subprocess.Popen("exec " + "~/ngrok tcp 22", stdout=subprocess.PIPE, shell=True)
             self.database['ngrok-status'] = 'on'
-        response = requests.get('http://localhost:4040/api/tunnels')
+        response = json.loads(requests.get('http://localhost:4040/api/tunnels').text)
         pub_url = response['tuneels'][0]['public_url']
         response_text = """
         @{}\nPublic URL:{}

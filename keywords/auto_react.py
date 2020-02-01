10c8a9cbb46ded737582ebc672451003961e4481
Ratana <ratanawang8@gmail.com>
2020-01-31 23:48:23 -0800

    Add requested changes: self.database['auto']

--- keywords/auto_react.py
+++ keywords/auto_react.py
@@ -1,16 +1,14 @@
 from commands import react
 from keywords.keyword import Keyword
-from fbchat import Message, MessageReaction
-from fbchat import Mention
-
-
-def auto_not():
-    if react.auto == "off":
-        return "on"
-    return "off"
+from fbchat import MessageReaction
 
 
 class auto_react(Keyword):
+
+    def auto_not(self):
+        if self.database["auto"] == "off":
+            return "on"
+        return "off"
 
     def find_reaction_emoji(self):
         msg = self.message_object.text.split()
@@ -21,21 +19,14 @@ class auto_react(Keyword):
                 return MessageReaction(emoji)
 
     def run(self):
-        if react.auto == "off":
+        if self.database["auto"] == "off":
             exit()
         reply_id = react.find_reply_id(self.message_object)
         self.client.reactToMessage(reply_id, self.find_reaction_emoji())
-        # response_text = "@" + self.author.first_name + " successful!"
-        # mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
-        # self.client.send(
-        #     Message(text=response_text, mentions=mentions),
-        #     thread_id=self.thread_id,
-        #     thread_type=self.thread_type
-        # )
 
     def define_documentation(self):
         self.documentation = {
             "trigger": "Words that make a bot emotional.",
             "function": "Allows Dentaku to react automatically at trigger words."
-                        + "\n\nTo turn auto_react {}, type `!react auto {}`".format(auto_not(), auto_not())
+                        + "\n\nTo turn auto_react {}, type `!react auto {}`".format(self.auto_not(), self.auto_not())
         }

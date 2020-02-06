c10a156938250d08c07e5e4fd52938b79cfc3615
Andy Shen <39166305+InhoStudios@users.noreply.github.com>
2020-02-05 21:54:14 -0800

    Add reply functionality to search command (#158)

--- commands/search.py
+++ commands/search.py
@@ -14,6 +14,7 @@ class search(Command):
 		index = 1;
 		query = ""
 
+
 		for i in self.user_params:
 			query += i
 			if index < length:
@@ -22,10 +23,28 @@ class search(Command):
 
 		j = ""
 
-		for j in googlesearch.search(query, tld="ca", num=1, stop=1, pause=2): 
-			print(j) 
+		if length > 0:
+			for j in googlesearch.search(query, tld="ca", num=1, stop=1, pause=2):
+				print(j)
 
-		response_text = "Here is Google's top search! " + j
+		j += ""
+
+		k = ""
+		query = ""
+		# get message from a replied thread
+		target = self.message_object.replied_to
+		if target is not None:
+			query = target.text
+		if query != "":
+			for k in googlesearch.search(query, tld="ca", num=1, stop=1, pause=2):
+				print(k)
+
+		if k == "" and j == "":
+			response_text = "Please input a search term"
+		elif k == "" or j == "":
+			response_text = "Here is Google's top search! " + j + k
+		else:
+			response_text = "Here are Google's top searches for those terms!\n" + j + "\n" + k
 		
 		self.client.send(
 			Message(text=response_text),
@@ -35,6 +54,6 @@ class search(Command):
 
 	def define_documentation(self):
 		self.documentation = {
-			"parameters": "SEARCH_TERM / SEARCH TERMS",
+			"parameters": "SEARCH_TERM / SEARCH TERMS / REPLY TO MESSAGE",
 			"function": "Searches Google and gives you the best result for your search"
 		}

ea3515513a5e54caadbc2571fe84a0fcc9768bd6
GcWan <GcWan@users.noreply.github.com>
2020-02-18 20:45:16 -0800

    Population in !weather

--- commands/weather.py
+++ commands/weather.py
@@ -22,14 +22,22 @@ class weather(Command):
         response_text = "@" + self.author.first_name
         if len(self.user_params) == 0:
             response_text += " Please enter a city"
-
+        elif "show" in self.user_params:
+            try:
+                url = "http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&APPID=6f68045e525e16f8232fb0e5f19987c4".format("%20".join(self.user_params[:self.user_params.index("show")]))
+                jsonurl = urlopen(url)
+                info = json.loads(jsonurl.read())
+                response_text += " " + info['city']['name'] + ", " + info['city']['country']
+                if self.user_params[self.user_params.index("show")+1].lower() == "population":
+                    response_text += "\nPopulation: " + str(info['city']['population'])
+            except:
+                response_text += " No command found"
         elif self.user_params[0].lower() != "help":
             try:
                 url = "http://api.openweathermap.org/data/2.5/forecast?q={}&units=metric&APPID=6f68045e525e16f8232fb0e5f19987c4".format("%20".join(self.user_params))
                 jsonurl = urlopen(url)
                 info = json.loads(jsonurl.read())
-                response_text += " Weather at "+ info['city']['name'] + ", " + info['city']['country'] +\
-                    "\nPopulation: " + str(info['city']['population'])
+                response_text += " Weather at "+ info['city']['name'] + ", " + info['city']['country']
                 info = info['list'][0]
                 response_text += "\nCurrent temperature: " + str(info['main']['temp']) + "ºC"
                 response_text += "\n\nMore info at "
@@ -53,7 +61,7 @@ class weather(Command):
                 response_text = "@" + self.author.first_name + " Dude is that even a place."
             response_text += " \nFor a full list of commands type !weather help"
         else:
-            response_text += " Possible Commands to add after the city:\nsunset, sunrise"
+            response_text += " You may also type \"show\" followed by:\npopulation, sunset, sunrise"
 
         self.client.send(
             Message(text=response_text, mentions= mentions),

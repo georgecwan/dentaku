65bffb2abb14e6b08e7002fa845aa50f809ebf23
georgecwan <georgecwan@users.noreply.github.com>
2020-03-27 23:05:56 -0700

    Added other locations to covid.py

--- commands/covid.py
+++ commands/covid.py
@@ -8,7 +8,10 @@ from datetime import date, timedelta
 class covid(Command):
 
     def run(self):
-        location = "Canada"
+        if len(self.user_params) == 0:
+            location = "Canada"
+        else:
+            location = " ".join(self.user_params)
         yesterday = str(date.today() - timedelta(days=1))[5:] + "-" + str(date.today() - timedelta(days=1))[:4]
         now = str(date.today())[5:] + "-" + str(date.today())[:4]
         try:
@@ -34,14 +37,14 @@ class covid(Command):
         deaths = 0
         recovered = 0
         for i in countries:
-            if i == location:
+            if i.lower() == location.lower():
                 rows.append(tindex)
             tindex += 1
         for i in rows:
             confirmed += list(response.loc[i])[2]
             deaths += list(response.loc[i])[3]
             recovered += list(response.loc[i])[4]
-        response_text = "@" + self.author.first_name + " Current COVID-19 numbers for " + location + ":\n"+ \
+        response_text = "@" + self.author.first_name + " Current COVID-19 numbers for " + countries[tindex] + ":\n"+ \
             "Comfirmed: " + confirmed + "\nDeaths: " + deaths + "\nRecovered: " + recovered
         mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
 

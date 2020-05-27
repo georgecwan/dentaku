5cc4c8663f782946d0ca63451491a8be10f1e8f4
georgecwan <georgecwan@users.noreply.github.com>
2020-05-26 20:56:46 -0700

    Add swear counter

--- keywords/language.py
+++ keywords/language.py
@@ -7,9 +7,19 @@ class language(Keyword):
 
     def run(self):
         if self.client.uid != self.author_id:
+            if 'swears' not in self.thread_data:
+                self.thread_data['swears'] = {}
+            swearDict = self.thread_data['swears']
+            if self.author_id not in swearDict:
+                swearDict[self.author_id] = 1
+            else:
+                swearDict[self.author_id] += 1
+
+            response_text = "@{} WATCH YOUR LANGUAGE\nSwear Count: {}"\
+                .format(self.author.first_name.upper(),swearDict[self.author_id])
             mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
             self.client.send(
-                Message(text="@" + self.author.first_name.upper() + " WATCH YOUR LANGUAGE", mentions=mentions, reply_to_id=self.message_object.uid),
+                Message(text=response_text, mentions=mentions, reply_to_id=self.message_object.uid),
                 thread_id=self.thread_id,
                 thread_type=self.thread_type
             )

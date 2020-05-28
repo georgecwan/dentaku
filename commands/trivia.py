2fed8079c910d573a807b4a72e29247539dcdc5b
georgecwan <georgecwan@users.noreply.github.com>
2020-05-28 16:11:16 -0700

    Add differing points for difficulty in !trivia

--- commands/trivia.py
+++ commands/trivia.py
@@ -26,6 +26,11 @@ class trivia(Command):
                     response_text += "\n{}: {}".format(self.getName(id), count)
             else:
                 response_text = "Nobody has answered yet. Try sending !trivia to begin."
+        elif len(self.user_params) > 0 and self.user_params[0].lower() == "help":
+            response_text = '''Possible commands:
+Easy, Medium, Hard: Sets difficulty of the question.
+
+Add the command after !trivia to use them.'''
         elif len(self.user_params) > 0 and self.thread_data['triviaAnswer'] != "n/a":
             # Receives the answer for the question
             mentions = [Mention(self.author_id, length=len(self.author.first_name) + 1)]
@@ -34,7 +39,7 @@ class trivia(Command):
                 self.thread_data['trivia'][self.author_id] = 0
             if self.thread_data['triviaAnswer'][0] == self.user_params[0].lower():
                 response_text += "Congrats! You got it right!"
-                self.thread_data['trivia'][self.author_id] += 1
+                self.thread_data['trivia'][self.author_id] += self.thread_data['triviaValue']
             else:
                 response_text += "Oof, the answer was actually " + self.thread_data['triviaAnswer']
             response_text += "\nYou now have {} points.".format(self.thread_data['trivia'][self.author_id])
@@ -42,7 +47,7 @@ class trivia(Command):
             self.save_db()
         else:
             # Generates question to send
-            if len(self.user_params) > 0 and self.user_params[0].lower in ["easy", "medium", "hard"]:
+            if len(self.user_params) > 0 and self.user_params[0].lower() in ["easy", "medium", "hard"]:
                 url = "https://opentdb.com/api.php?amount=1&difficulty=" + self.user_params[0].lower()
             else:
                 url = "https://opentdb.com/api.php?amount=1"
@@ -54,6 +59,12 @@ class trivia(Command):
             response_text = """Category: {}
 Difficulty: {}
 Q: {}""".format(info['category'], info['difficulty'], info['question'])
+            if info['difficulty'] == "easy":
+                self.thread_data['triviaValue'] = 1
+            elif info['difficulty'] == "medium":
+                self.thread_data['triviaValue'] = 2
+            else:
+                self.thread_data['triviaValue'] = 3
             choices = [info['correct_answer']]
             for c in info['incorrect_answers']:
                 choices.append(c)
@@ -72,6 +83,7 @@ Q: {}""".format(info['category'], info['difficulty'], info['question'])
             for c in choices:
                 response_text += "\n"+c
             response_text += "\n\nTo answer, type !trivia followed by the letter of your choice."
+            response_text += "\nTo learn how to customize the question, type '!trivia help'"
 
         self.client.send(
             Message(text=response_text, mentions=mentions),
@@ -81,6 +93,6 @@ Q: {}""".format(info['category'], info['difficulty'], info['question'])
 
     def define_documentation(self):
         self.documentation = {
-            "parameters": "None / ANSWER / [ranking/reset]",
+            "parameters": "None / ANSWER/ 'help' / [ranking/reset]",
             "function": "Asks a trivia question."
         }
